74788eb6052908dba3433fcefc4aafc0840ab6e7
wjf1995 <1072016060@qq.com>
2017-12-14 11:20:22 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/Weather.java
@@ -0,0 +1,8 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/12/13 0013.
+ */
+
+public class Weather {
+}
---
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -0,0 +1,8 @@
+package com.coolweather.android.service;
+
+/**
+ * Created by Administrator on 2017/12/14 0014.
+ */
+
+public class AutoUpdateService {
+}
